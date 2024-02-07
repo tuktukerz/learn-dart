3af76f54ab7968caec1329ad65e9b06f93b9527c
tuktukerz <aflianmahyaruddin@gmail.com>
2024-02-07 21:26:10 +0700

    learn(day1) : object

--- bin/learn.dart
+++ bin/learn.dart
@@ -1,11 +1,17 @@
 void main() {
- int age = 23;
- double height = 1.75;
- String name = 'Aflian';
- bool isWorking = false;
- String locationWorking = 'Satu Dental';
+  var name = 'Test';
+  var newObjects = ['Satu', 'Dua', 'Tiga'];
+  var dummyData = {
+    'users' : {
+      'Name' : 'Aflian',
+    },
+    'Hobbies' : {
+      'Office' : ['Futsal', 'Badminton'],
+      'Home' : ['Reading'],
+    },
+  };
 
- print('My name is $name');
- print('I am $age years old and height $height meters');
- print('Are you working ? ${isWorking ? "I am working on $locationWorking" : "I am not a working"}');
+  print(name);
+  print(newObjects);
+  print(dummyData);
 }
