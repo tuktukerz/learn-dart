d95a3b90f6a0190965cd77cdbfc6e419b68b3d4a
tuktukerz <aflianmahyaruddin@gmail.com>
2024-02-07 21:16:11 +0700

    learn(day1) : install & conditions

---
+++ bin/learn.dart
@@ -0,0 +1,11 @@
+void main() {
+ int age = 23;
+ double height = 1.75;
+ String name = 'Aflian';
+ bool isWorking = false;
+ String locationWorking = 'Satu Dental';
+
+ print('My name is $name');
+ print('I am $age years old and height $height meters');
+ print('Are you working ? ${isWorking ? "I am working on $locationWorking" : "I am not a working"}');
+}
